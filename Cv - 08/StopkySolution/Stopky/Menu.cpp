21b53cdac051ea57a7ba4192ac5055bc066b88d5
TobMit <80719513+TobMit@users.noreply.github.com>
2022-11-07 13:04:13 +0100

    Stopky dokončené

--- Cv - 08/StopkySolution/Stopky/Menu.cpp
+++ Cv - 08/StopkySolution/Stopky/Menu.cpp
@@ -1,7 +1,9 @@
-// Zakladna aplikacia
+﻿// Zakladna aplikacia
 
 #include <windows.h>
 #include <windowsx.h>
+#include "resource.h"
+#include <ctime>
 
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 
@@ -23,7 +25,7 @@ int APIENTRY WinMain(HINSTANCE hThisInst, HINSTANCE hPrevInst, PSTR lpszArgs, in
 	wndclass.hIconSm = 0; 		// Druh malej ikony
 
 	wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);     // Druh kurzora
-	wndclass.lpszMenuName = NULL;                            // Ziadne menu
+	wndclass.lpszMenuName = MAKEINTRESOURCE(IDR_MENU1);                            // Ziadne menu
 
 	wndclass.cbClsExtra = 0;          	// nie su potrebne
 	wndclass.cbWndExtra = 0;          	// ziadne extra informacie
@@ -63,16 +65,59 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
 	HDC hdc;
 	PAINTSTRUCT ps;
 	RECT	  rect;
+	static time_t startCas, aktualnyCas;
+	static HMENU hmenu;
+	static bool casovacAktivny = false;
 
 	switch (message) {
+	case WM_CREATE:
+		hmenu = GetMenu(hwnd);
+		break;
 	case WM_PAINT:
 		hdc = BeginPaint(hwnd, &ps);
-		GetClientRect(hwnd, &rect);
-		DrawText(hdc, "AHOJ WINDOWS !!! ", -1, &rect, DT_SINGLELINE | DT_CENTER | DT_VCENTER);
+		{ // zavolá sa alokácia / inicializácia objekut
+			GetClientRect(hwnd, &rect);
+			int difCas = aktualnyCas - startCas;
+			char casBuf[20];
+			wsprintf(casBuf,"%02d:%02d:%02d", difCas/3600, (difCas%3600)/60, (difCas%3600%60)); // funkcia windowsu má výhodu oproti sprintf, je uložená v dll
+			DrawText(hdc, casBuf, -1, &rect, DT_SINGLELINE | DT_VCENTER | DT_CENTER);
+		} // volá sa deštruktor objektu
 		EndPaint(hwnd, &ps);
+		break;
+	case WM_COMMAND:
+		switch (LOWORD(wParam))
+		{
+		case ID_PROGRAM_KONIEC:
+			PostMessage(hwnd, WM_CLOSE, 0, 0);
+				break;
+		case ID_PROGRAM_40005: //toto by malo byť štart
+			casovacAktivny = true;
+			startCas = aktualnyCas = time(nullptr);
+			InvalidateRect(hwnd,nullptr, true);
+			SetTimer(hwnd, 1, 1000, nullptr); // keď je tam nullptr správa ide do oknovej procedúri
+			EnableMenuItem(hmenu, ID_PROGRAM_40005, MF_DISABLED);
+			EnableMenuItem(hmenu, ID_PROGRAM_STOP, MF_ENABLED);
+			break;
+
+		case ID_PROGRAM_STOP:
+			KillTimer(hwnd,1);
+			casovacAktivny = false;
+			EnableMenuItem(hmenu, ID_PROGRAM_40005, MF_ENABLED);
+			EnableMenuItem(hmenu, ID_PROGRAM_STOP, MF_DISABLED);
+			break;
+		}
+		break;
+
+	case WM_TIMER:
+		aktualnyCas = time(nullptr);
+		InvalidateRect(hwnd, nullptr, true);
 		break;
 
 	case WM_DESTROY:
+		if (casovacAktivny)
+		{
+			KillTimer(hwnd, 1);
+		}
 		PostQuitMessage(0);
 		break;
 	default:
