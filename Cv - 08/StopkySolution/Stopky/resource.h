5781960c7bc0129112879089b67b995fc6ffe837
TobMit <80719513+TobMit@users.noreply.github.com>
2022-11-07 13:14:59 +0100

    Pridanie akcelerátorov

--- Cv - 08/StopkySolution/Stopky/resource.h
+++ Cv - 08/StopkySolution/Stopky/resource.h
@@ -3,6 +3,7 @@
 // Used by Resource.rc
 //
 #define IDR_MENU1                       101
+#define IDR_ACCELERATOR1                102
 #define ID_PROGRAM_STOP                 40001
 #define ID_PROGRAM_                     40002
 #define ID_PROGRAM_OAPLIK40003          40003
@@ -13,8 +14,8 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        102
-#define _APS_NEXT_COMMAND_VALUE         40006
+#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_COMMAND_VALUE         40009
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
