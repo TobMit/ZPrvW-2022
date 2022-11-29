8b39c59ea5d8a7470ba551e2a846b4a3ff3e9086
TobMit <80719513+TobMit@users.noreply.github.com>
2022-11-29 07:19:46 +0100

    Resources sú hotová

--- Cv-11/SchrankaSolution/Schranka/resource.h
+++ Cv-11/SchrankaSolution/Schranka/resource.h
@@ -1,13 +1,22 @@
 //{{NO_DEPENDENCIES}}
 // Microsoft Visual C++ generated include file.
 // Used by Schranka.rc
+//
+#define IDR_MENU1                       101
+#define IDR_ACCELERATOR1                102
+#define ID_PROGRAM_KONIEC               40001
+#define ID_EDIT_VLOZ                    40002
+#define ID_EDIT_KOPIRUJ                 40003
+#define ID_EDIT_FORMAT                  40004
+#define ID_ACCELERATOR40007             40007
+#define ID_EDIT_ZMAZ                    40008
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        101
-#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_COMMAND_VALUE         40010
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
